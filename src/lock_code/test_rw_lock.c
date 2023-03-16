c930efe17b2477e679d62b94579cb958cdb39372
chenzhiyin <zhiyin.chen@intel.com>
2023-03-15 22:12:31 -0400

    fix

--- src/lock_code/test_rw_lock.c
+++ src/lock_code/test_rw_lock.c
@@ -149,7 +149,7 @@ void sig_handler(int signo)
 			printf("\nThe thread %d,count:%d\n",i,thread_param[i].count);
 		}//same as no-binding core test
 		printf("lock_counter=%ld\n", lock_counter);
-		clock_gettime(CLOCK_MONOTONIC, &tm_ended);  // Get the monotonic time at the end of the thread      author£ºhh
+		clock_gettime(CLOCK_MONOTONIC, &tm_ended);  // Get the monotonic time at the end of the thread      authorï¿½ï¿½hh
 		printf("starttime:sec:%lld,nsec:%lld\n",tm_start.tv_sec,tm_start.tv_nsec);
 		printf("endtime:sec:%lld,nsec:%lld\n",tm_ended.tv_sec,tm_ended.tv_nsec);
 		printf("running time:%lld\n",(tm_ended.tv_sec-tm_start.tv_sec)*1000000000+tm_ended.tv_nsec-tm_start.tv_nsec);
@@ -257,7 +257,7 @@ int main(int argc, char *argv[])
 	}
 	
 	lock_counter = nr_rd_lock + nr_wr_lock;
-    printf("%lld,%lld,%lld,",nr_rd_lock/exe_duration,nr_wr_lock/exe_duration,lock_counter/exe_duration);
+    printf("%lld %lld %lld ",nr_rd_lock/exe_duration,nr_wr_lock/exe_duration,lock_counter/exe_duration);
 
     return 0;
 }
